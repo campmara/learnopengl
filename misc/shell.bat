f9e8ece64d742de8fc99ed70d3b587fa9a20d0db
Mara Campbell <mara@galliumstudios.com>
2025-05-11 10:50:04 -0700

    added a misc folder

---
+++ misc/shell.bat
@@ -0,0 +1,5 @@
+@echo off
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set path=c:\work\cpp\learning\learnopengl\misc;%path%
+cd "C:\work\cpp\learning\learnopengl\"
+call cls
